ff459dac8046b7ac0e5dde52190ea35c1f743a72
Aaron Choi Hao Ming <2405070J@student.tp.edu.sg>
2026-01-22 10:52:35 +0800

    Delete SWAP_PROJECT directory

--- SWAP_PROJECT/sanitize.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Content-Security-Policy: default-src 'self'; script-src 'self'");
-function sanitize_input($input) {
-    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-}
--- SWAP_PROJECT/security.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Security-Policy: default-src 'self'; script-src 'self'");
-function validate_input($input) {
-    if (!is_string($input)) return false;
-    if (strlen($input) > 500) return false;
-    return true;
-}
